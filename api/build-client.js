7ad1adeed57203a51d278cc6e8bf6dde57027e3c
Omar Malavé <omarmalave21@gmail.com>
2020-11-29 18:24:53 -0400

    host fix

--- api/build-client.js
+++ api/build-client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const buildClient = ({ req }) => {
   const onServer = typeof window === 'undefined';
-  const baseURL = onServer ? 'http://ingress-nginx-controller.kube-system.svc.cluster.local' : '';
+  const baseURL = onServer ? 'http://www.ticketing-app.cloud' : '';
   return axios.create({
     baseURL,
     headers: req?.headers || {},
